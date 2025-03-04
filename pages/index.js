b00344f3d67f9874146a5ab27f7a670309463f88
jpolsley <youngandsanctified@gmail.com>
2025-03-04 06:09:06 -0600

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <div style={{
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      height: "100vh",
+      backgroundColor: "#000",
+      color: "#fff",
+      fontFamily: "Arial, sans-serif",
+      textAlign: "center"
+    }}>
+      <h1>Notion Progress Widget</h1>
+    </div>
+  );
+}
