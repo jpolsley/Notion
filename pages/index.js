f3ee05e82f548da8c0e310deab6f1fa9d65bc05e
jpolsley <youngandsanctified@gmail.com>
2025-10-15 11:38:41 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,1 @@
-export default function Home() {
-  return (
-    <div style={{
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
-      height: "100vh",
-      backgroundColor: "#000",
-      color: "#fff",
-      fontFamily: "Arial, sans-serif",
-      textAlign: "center"
-    }}>
-      <h1>Notion Progress Widget</h1>
-    </div>
-  );
-}
+
